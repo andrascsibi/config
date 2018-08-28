a91b7cab094ebfea9bbb907ce546b3056d096f9c
Andras Csibi <andras.csibi@gmail.com>
2018-08-28 10:29:26 +0200

    🎉 added install script

---
+++ install.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update homebrew recipes
+brew update
+
+# Install GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+
+# Install Bash 4
+brew install bash
+
+# Install more recent versions of some OS X tools
+brew install grep
+
+# Apps
+apps=(
+  java8
+)
+
+# Install apps to /Applications
+# Default is: /Users/$user/Applications
+echo "installing apps..."
+brew cask install --appdir="/Applications" ${apps[@]}
+
+PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
+
+binaries=(
+  node
+  git
+  liquidprompt
+)
+echo "installing binaries..."
+brew install ${binaries[@]}
+
+brew cleanup
+
+brew tap caskroom/cask
+brew tap caskroom/versions
+
+casks=(
+  slack
+  discord
+)
+echo "installing casks"
+brew cask install ${casks[@]}
+
+brew cask cleanup
