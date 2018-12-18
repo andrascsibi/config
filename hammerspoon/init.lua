6e27ee6dbbb7407e7ce0c01a41d6959f1f6d3312
Andras Csibi <andras.csibi@gmail.com>
2018-12-18 13:08:04 +0100

    ✨ add hammerspoon conf

---
+++ hammerspoon/init.lua
@@ -0,0 +1,47 @@
+local hyper = { "cmd", "alt", "ctrl", "shift" }
+
+
+local applicationHotkeys = {
+  s = 'Sublime Text',
+  c = 'Google Chrome',
+  a = 'iTerm',
+}
+for key, app in pairs(applicationHotkeys) do
+  hs.hotkey.bind(hyper, key, function()
+    hs.application.launchOrFocus(app)
+  end)
+end
+
+hs.hotkey.bind(hyper, "0", function()
+  hs.reload()
+end)
+
+hs.hotkey.bind(hyper, "9", function()
+  hs.notify.new({title="Fucker", informativeText="get back to work"}):send()
+end)
+
+hs.notify.new({title="Hammerspoon", informativeText="Config loaded"}):send()
+
+
+hs.window.animationDuration = 0
+hs.hotkey.bind(hyper, "left", function()
+  local win = hs.window.focusedWindow();
+  if not win then return end
+win:moveToUnit(hs.layout.left50)
+end)
+hs.hotkey.bind(hyper, "up", function()
+  local win = hs.window.focusedWindow();
+  if not win then return end
+win:moveToUnit(hs.layout.maximized)
+end)
+hs.hotkey.bind(hyper, "down", function()
+  local win = hs.window.focusedWindow();
+  if not win then return end
+win:moveToScreen(win:screen():next())
+end)
+hs.hotkey.bind(hyper, "right", function()
+  local win = hs.window.focusedWindow();
+  if not win then return end
+win:moveToUnit(hs.layout.right50)
+end)
+
